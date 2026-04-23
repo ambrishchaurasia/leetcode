3f1a70644d22dd69bb2551a3953bc850d0a035c0
Ambrish Chauraiya <ambrishchaurasia2003@gmail.com>
2026-04-23 17:30:34 +0530

    Time: 4 ms (99.83%) | Memory: 63.6 MB (28.44%) - LeetSync

---
+++ 820-find-eventual-safe-states/find-eventual-safe-states.java
@@ -0,0 +1,57 @@
+class Solution {
+    public List<Integer> eventualSafeNodes(int[][] graph) {
+        boolean vis[]=new boolean[graph.length];
+        boolean pathvis[]=new boolean[graph.length];
+        boolean eventualsafe[]=new boolean[graph.length];
+        List<Integer> ans=new ArrayList<>();
+        for(int i=0;i<graph.length;i++)
+        {
+            if(!vis[i])
+            {
+            if(dfs(graph,i,vis,pathvis,eventualsafe))
+            eventualsafe[i]=false;
+            else
+            eventualsafe[i]=true;
+
+
+            }
+        }
+        for(int i=0;i<graph.length;i++)
+        {
+            if(eventualsafe[i])
+            ans.add(i);
+        }
+        return ans;
+
+    }
+        boolean dfs(int[][] graph,int i,boolean [] vis,boolean []pathvis,boolean []eventualsafe)
+        {
+             vis[i]=true;
+             pathvis[i]=true;
+            for(int x=0;x<graph[i].length;x++)
+            {
+                if(!vis[graph[i][x]])
+                {
+                    if(dfs(graph,graph[i][x],vis,pathvis,eventualsafe))
+                    {
+                    eventualsafe[graph[i][x]]=false;
+                    return true;
+                    }
+                    else
+                    {
+                    eventualsafe[i]=true;
+
+                    }
+                }
+                else if(pathvis[graph[i][x]])
+                {
+                    eventualsafe[graph[i][x]]=false;
+                    return true;
+                }
+            }
+                pathvis[i]=false;
+                 eventualsafe[i] = true;
+                return false;
+
+        }
+}
