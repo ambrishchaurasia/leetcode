71365fec03cfff6cad4bd60757ed3dd69cc57070
Ambrish Chauraiya <ambrishchaurasia2003@gmail.com>
2026-05-03 03:15:59 +0530

    Time: 4 ms (22.52%) | Memory: 47.6 MB (69.59%) - LeetSync

--- 135-candy/candy.java
+++ 135-candy/candy.java
@@ -1,38 +1,42 @@
+//slope approach
 class Solution {
     public int candy(int[] ratings) {
-        int l[]=new int[ratings.length];
-        l[0]=1;
-         for(int i=1;i<ratings.length;i++)
-         {
-            if(ratings[i-1]<ratings[i])
+        int down;
+        int peak;
+        int sum=1;
+        int i=1;
+        if(ratings.length==1)
+        return 1;
+        boolean p=false;
+        while(i<ratings.length)
+        {
+            while(i<ratings.length && ratings[i]==ratings[i-1])
             {
-                l[i]=l[i-1]+1;
+
+                sum=sum+1;
+                i++;
             }
-            else
-            l[i]=1;
-         }
-
-         int curr=0;
-         int right=0;
-         right=1;
-         int sum=0;
-        sum=sum+l[ratings.length-1];
-
-        for(int i=ratings.length-2;i>=0;i--)
-         {
-            if(ratings[i]>ratings[i+1])
-            {
-                curr=1+right;
-                right=curr;
-            }
-            else{
-            right=1;
-            curr=1;
-            }
-
-            sum=sum+Math.max(l[i],curr);
-         }
-         return sum;
-
+            peak=1;
+          while(i<ratings.length && ratings[i-1]<ratings[i])
+          {
+             p=true;
+            i++;
+            peak++;
+            sum=sum+peak;
+          }
+          down=0;
+          while(i<ratings.length &&  ratings[i-1]>ratings[i])
+          {
+            i++;
+            down++;
+            sum=sum+down;
+          }
+          down++;
+          if(peak<down )
+          {
+            sum=sum+down-peak;
+          }
+        }
+        return sum;
     }
 }
