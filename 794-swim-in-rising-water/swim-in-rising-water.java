7cddecb2eee3c84066584aa45599586da0ef3c32
Ambrish Chauraiya <ambrishchaurasia2003@gmail.com>
2026-06-15 23:42:57 +0530

    Time: 8 ms (89.67%) | Memory: 46.5 MB (31.85%) - LeetSync

--- 794-swim-in-rising-water/swim-in-rising-water.java
+++ 794-swim-in-rising-water/swim-in-rising-water.java
@@ -1,42 +1,92 @@
+class DisjointSet
+{
+    int []ulp;
+    int []rank;
+    DisjointSet(int n)
+    {
+     ulp=new int[n];
+     rank=new int[n];
+     for(int i=0;i<n;i++)
+     {
+        ulp[i]=i;
+        rank[i]=1;
+     }
+    }
+    int findulp(int n)
+    {
+        if(ulp[n]==n)
+        return n;
+
+        int up=findulp(ulp[n]);
+        ulp[n]=up;
+        return up;
+    }
+    void ubr(int u,int v)
+    {
+     int uu = findulp(u);
+     int uv = findulp(v);
+        if(uu==uv)
+        return;
+        
+        int ru=rank[uu];
+        int rv=rank[uv];
+        if(ru>rv)
+        {
+            ulp[uv]=uu;
+        }
+        else if(rv>ru)
+        {
+            ulp[uu]=uv;
+        }
+        else
+        {
+            ulp[uu]=uv;
+            rank[uv]=rank[uv]+1;
+        }
+    }
+}
 class Solution {
     public int swimInWater(int[][] grid) {
-        //min wt path from 0 to n-1 
-        PriorityQueue <int[]> pq =new PriorityQueue<>((a,b)->Integer.compare(a[2],b[2]));
-        int[] r={0,-1,0,1};
-        int[] c={-1,0,1,0};
+        int []r={0,-1,0,1};
+        int []c={-1,0,1,0};
         int n=grid.length;
-                    boolean vis[][]=new boolean [n][n];
-                    int min_maxtime=Integer.MAX_VALUE;
-        pq.offer(new int[]{0,0,grid[0][0]});
-        while(!pq.isEmpty())
+        int pos[][]=new int[n*n][2];
+
+        for(int i=0;i<grid.length;i++)
         {
-            int a[]=pq.poll();
-            int row=a[0];
-            int col=a[1];
-            int time=a[2];
-            if(vis[row][col])
-            continue;
-
-            vis[row][col]=true;
-
-            for(int i=0;i<r.length;i++)
+            for(int j=0;j<grid.length;j++)
             {
-                int adjr=row+r[i];
-                int adjc=col+c[i];
-                if(adjr >=0 && adjr<n && adjc>=0 && adjc<n)
-                {
-                int adjtime=grid[adjr][adjc];
-                
-                if(adjr ==n-1 && adjc==n-1 )
-                return Math.max(time,adjtime);
-                if(!vis[adjr][adjc])
-                {
-                    pq.offer(new int[]{adjr,adjc,Math.max(time,adjtime)});
-                }
-                }
+                pos[grid[i][j]]=new int[]{i,j};
             }
-
         }
-        return 0;
-    }
+        DisjointSet ds=new DisjointSet(n*n);
+        boolean [][]active =new boolean[n][n];
+        for(int i=0;i<n*n;i++)
+        {
+            int a[]=pos[i];
+            int ro=a[0];
+            int co=a[1];
+            int node=ro*n+co;
+            int lastnode=n*n -1;
+            active[ro][co]=true;
+            for(int j=0;j<r.length;j++)
+            {
+                int adjr=ro+r[j];
+                int adjc=co+c[j];
+                int adjnode=adjr*n+adjc;
+                if(adjr>=0 && adjr<n && adjc >=0 && adjc<n)
+                {
+                if(active[adjr][adjc])
+                {
+                   int ulp1 =ds.findulp(node);
+                    int ulp2=ds.findulp(adjnode);
+                    if(ulp1!=ulp2)
+                    {
+                        ds.ubr(ulp1,ulp2);
+                    }
+                    if(ds.findulp(0)==ds.findulp(lastnode))
+                    return i;
+                }}} }
+                    return 0;
+                      }
 }
