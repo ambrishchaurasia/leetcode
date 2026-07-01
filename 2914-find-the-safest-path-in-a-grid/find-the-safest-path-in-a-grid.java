4fd8195fa36139326272b2b17cb97f513fe4c03c
Ambrish Chauraiya <ambrishchaurasia2003@gmail.com>
2026-07-01 17:49:18 +0530

    Time: 363 ms (8.25%) | Memory: 123.8 MB (30.16%) - LeetSync

--- 2914-find-the-safest-path-in-a-grid/find-the-safest-path-in-a-grid.java
+++ 2914-find-the-safest-path-in-a-grid/find-the-safest-path-in-a-grid.java
@@ -77,7 +77,10 @@ class Solution {
                  if(newr>=0 && newr<grid.size() && newc>=0 && newc<grid.get(0).size())
                  {
                 int newdis=grid.get(newr).get(newc);
+                
                 int distance=Math.min(dis,newdis);
+                if((newr==grid.size()-1) && (newc==grid.size()-1))
+                return distance;
                 if(!vis[newr][newc] )
                 {
                     pq.offer(new Pair(newr,newc,distance));
