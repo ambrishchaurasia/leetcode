9867c278dadd3815709dffccde72590dcc9eb960
Ambrish Chauraiya <ambrishchaurasia2003@gmail.com>
2026-07-29 13:11:12 +0530

    Time: 271 ms (5.61%) | Memory: 65.8 MB (8.73%) - LeetSync

---
+++ 131-palindrome-partitioning/palindrome-partitioning.java
@@ -0,0 +1,48 @@
+
+class Solution {
+
+    public List<List<String>> partition(String s) {
+            List<List<String>> ans=new ArrayList<>();
+            List<String> curr=new ArrayList<>();
+            rec(ans,curr,s,0);
+            return ans;
+    }
+
+        void rec(List<List<String>> ans,List<String> curr,String s,int i)
+        {
+            if(i==s.length())
+            {
+                ans.add(new ArrayList<>(curr));
+                return;
+            }
+
+           for(int j=i;j<s.length();j++)
+           {
+            if(isPal(i,j,s))
+            {
+                curr.add(s.substring(i,j+1));
+                System.out.println(curr);
+                rec(ans,curr,s,j+1);
+                curr.remove(curr.size()-1);
+
+            }
+
+           }
+        }
+
+
+    boolean isPal(int i,int j,String s)
+    {
+        while(i<j)
+        {
+            if(s.charAt(i)==s.charAt(j))
+            {
+                i++;
+                j--;
+            }
+            else
+            return false;
+        }
+        return true;
+    }
+}
