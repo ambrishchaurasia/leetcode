cf7adbbf896d07ef215ea15e7697cf5c221a4267
Ambrish Chauraiya <ambrishchaurasia2003@gmail.com>
2026-08-04 01:59:28 +0530

    Time: 3 ms (86.46%) | Memory: 47.5 MB (69.41%) - LeetSync

--- 135-candy/candy.java
+++ 135-candy/candy.java
@@ -1,42 +1,37 @@
-//slope approach
 class Solution {
     public int candy(int[] ratings) {
-        int down;
-        int peak;
-        int sum=1;
-        int i=1;
-        if(ratings.length==1)
-        return 1;
-        boolean p=false;
-        while(i<ratings.length)
+        int i=0;
+        int ans=0;
+        int peak=1;
+        int peak2=1;
+        while(i<ratings.length-1)
         {
-            while(i<ratings.length && ratings[i]==ratings[i-1])
-            {
+            
 
-                sum=sum+1;
+             while(i<ratings.length-1 && ratings[i]==ratings[i+1])
+            {
+                ans=ans+1;
                 i++;
             }
-            peak=1;
-          while(i<ratings.length && ratings[i-1]<ratings[i])
-          {
-             p=true;
-            i++;
-            peak++;
-            sum=sum+peak;
-          }
-          down=0;
-          while(i<ratings.length &&  ratings[i-1]>ratings[i])
-          {
-            i++;
-            down++;
-            sum=sum+down;
-          }
-          down++;
-          if(peak<down )
-          {
-            sum=sum+down-peak;
-          }
+
+            peak= 1;
+            while(i<ratings.length-1  && ratings[i]<ratings[i+1])
+            {
+                ans=ans+peak;
+                peak++;
+                i++;
+            }
+           
+            peak2=1;
+            while(i<ratings.length-1 && ratings[i]>ratings[i+1])
+            {
+                ans=ans+peak2;
+                peak2++;
+                i++;
+            }
+            ans=ans+Math.max(peak,peak2)-1;
+           
         }
-        return sum;
+        return ans+1;
     }
 }
